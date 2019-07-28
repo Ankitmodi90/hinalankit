66ecbcca3126f35f5e52493190529cc1e07dce54
admodi <admodi@ADMODI-LAP.oradev.oraclecorp.com>
2019-07-28 18:30:45 +0530

    hello

--- hinalModi/src/test/java/hinalModi/hinla.java
+++ hinalModi/src/test/java/hinalModi/hinla.java
@@ -5,6 +5,8 @@ public class hinla {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hinal+ankit+1.1");
+
+System.out.println("hinal+ankit+1.2");
 	}
 
 }
